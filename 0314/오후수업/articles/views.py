950a593e29eb15bb98947176675e4aa2268cb20d
alsuddlek1 <minyoung99311@gmail.com>
2023-03-14 17:49:50 +0900

    0314---

---
+++ 0314/오후수업/articles/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+# view함수는 한가지 특이한 점이 있다.
+# 첫번째 인자는 반드시 request를 받도록 되어있다.
+def index(request):
+    # app_name
+    context = {
+        'num' : 30
+    }
+    return render(request, 'articles/index.html', context)
---
+++ Error.md
@@ -0,0 +1,5 @@
+# Error
+
+## 없는 경로일때
+
+![](C:\Users\SSAFY\AppData\Roaming\marktext\images\2023-03-14-15-42-25-image.png)
