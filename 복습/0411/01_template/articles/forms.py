a1f126f58d9346b7cd1972bf4b771075f0f9d1ff
minyoung <minyoung99311@gmail.com>
2023-04-12 23:15:47 +0900

    0412

--- 복습/0411/01_template/articles/forms.py
+++ 복습/0411/01_template/articles/forms.py
@@ -9,10 +9,10 @@ from .models import Article, Comment
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = '__all__'
+        fields = ('title', 'content',)
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         # fields = ('content',)
-        exclude = ('article', )
+        exclude = ('article', 'user',)
