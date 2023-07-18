9b743aebc10238ddfe1ad6b119140b457ff9b862
minyoung <minyoung99311@gmail.com>
2023-07-18 23:22:05 +0900

    신종혁바보

--- 트위터 클론/twitter/src/component/App.js
+++ 트위터 클론/twitter/src/component/App.js
@@ -5,7 +5,10 @@ function App() {
   const [isLoggedIn, setIsLoggendIn] = useState(true)
 
   return (
+    <div>
       <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; {new Date().getFullYear()}</footer>
+    </div>
   );
 }
 
