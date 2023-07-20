4b866c8b622bab1a9b82c60429d66637e1833fd6
minyoung <minyoung99311@gmail.com>
2023-07-21 00:13:57 +0900

    프로젝트 연습

---
+++ react_vlpt/practice/practice/src/components/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Home.css"
+
+function home(){
+    return (
+        <div className="home">
+            home에 들어갈 컨포넌트 만들기
+        </div>
+    )
+}
+
+export default home
